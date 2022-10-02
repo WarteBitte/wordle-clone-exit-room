f0e7c22c2685fd18c05d18a1f55ebf82dff03fad
WarteBitte <magicheiner@gmail.com>
2022-10-02 14:45:55 +0200

    funktionsweise hinzugefügt

--- script.js
+++ script.js
@@ -9,19 +9,24 @@ function wortEingegeben() {
 
         for (let i = 1; i < 6; i++) {
             document.getElementById(`w${anzahlWoerter}${i}`).innerHTML = eingegebenesWort.charAt(i - 1);
+            if (eingegebenesWort.charAt(i - 1).toLowerCase() == richtigesWort.charAt(i - 1)) {
+                document.getElementById(`w${anzahlWoerter}${i}`).style.backgroundColor = "#6aaa64";
 
+            } else if (richtigesWort.indexOf(eingegebenesWort.charAt(i - 1).toLowerCase()) != -1) {
+                document.getElementById(`w${anzahlWoerter}${i}`).style.backgroundColor = "#c9b458";
+            }
         }
 
         eingabefeld.value = "";
         console.log("Anzahl Wörter: " + anzahlWoerter);
 
-        if (eingegebenesWort == richtigesWort) {
+        if (eingegebenesWort.toLowerCase() == richtigesWort) {
             wortErraten = true;
 
             console.log("Wort erraten!")
 
             document.getElementById("eingabefeld").readOnly = true;
-            document.getElementById("eingabefeld").value = eingegebenesWort;
+            document.getElementById("eingabefeld").value = richtigesWort;
         }
     }
 }
